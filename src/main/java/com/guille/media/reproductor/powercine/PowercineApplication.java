ec72ebcfffef93541ccfdf5ce979cb71e7203202
Gabriel Liendo M. <gabrielliendo1257@gmail.com>
2026-06-28 15:20:54 -0500

    ...

--- src/main/java/com/guille/media/reproductor/powercine/PowercineApplication.java
+++ src/main/java/com/guille/media/reproductor/powercine/PowercineApplication.java
@@ -1,9 +1,10 @@
 package com.guille.media.reproductor.powercine;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @EnableCaching
